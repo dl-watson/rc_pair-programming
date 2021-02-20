334915f846ebe318a5b1ce4e8aaf462297f5102a
dl-watson <dlarkinwatson@gmail.com>
2021-02-20 14:01:14 -0800

    set up client in app.test.js

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,7 +1,16 @@
 const request = require("supertest");
 const app = require("../lib/app");
+const { client } = require("../lib/connection/client");
 
 describe("pair-programming routes", () => {
+  beforeAll(() => {
+    return client.set("testkey", "testval");
+  });
+
+  afterAll(() => {
+    return client.end(true);
+  });
+
   it("tests that a GET to the /get endpoint works as intended", () => {});
 
   it("tests that a GET to the /set endpoint works as intended", () => {});
