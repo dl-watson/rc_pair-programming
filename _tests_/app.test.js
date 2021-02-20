efdb028974f58842091f2bba70fd851dc0a24ae9
dl-watson <dlarkinwatson@gmail.com>
2021-02-20 13:55:14 -0800

    add empty tests

---
+++ _tests_/app.test.js
@@ -0,0 +1,8 @@
+const request = require("supertest");
+const app = require("../lib/app");
+
+describe("pair-programming routes", () => {
+  it("tests that a GET to the /get endpoint works as intended", () => {});
+
+  it("tests that a GET to the /set endpoint works as intended", () => {});
+});
